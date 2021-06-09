4c8c790fa4de40c57ea101ac9224520a0100427c
Krzysztof Bera <krzysztof.bera9@gmail.com>
2021-06-09 16:31:47 +0200

    basic working version

---
+++ amcom.c
@@ -0,0 +1,178 @@
+#include <string.h>
+#include <stdint.h>
+#include <stdio.h>
+#include "amcom.h"
+
+/// Start of packet character
+const uint8_t  AMCOM_SOP         = 0xA1;
+const uint16_t AMCOM_INITIAL_CRC = 0xFFFF;
+
+static uint16_t AMCOM_UpdateCRC(uint8_t byte, uint16_t crc)
+{
+	byte ^= (uint8_t)(crc & 0x00ff);
+	byte ^= (uint8_t)(byte << 4);
+	return ((((uint16_t)byte << 8) | (uint8_t)(crc >> 8)) ^ (uint8_t)(byte >> 4) ^ ((uint16_t)byte << 3));
+}
+
+
+void AMCOM_InitReceiver(AMCOM_Receiver* receiver, AMCOM_PacketHandler packetHandlerCallback, void* userContext) {
+	if (receiver == NULL)
+    {
+        return;
+    }
+    
+	receiver->packetHandler = packetHandlerCallback;
+	receiver->userContext = userContext;
+}
+
+size_t AMCOM_Serialize(uint8_t packetType, const void* payload, size_t payloadSize, uint8_t* destinationBuffer) {
+	// create handler to our packet header
+	AMCOM_PacketHeader packetHeaderHandler;
+	
+	// initialize it
+	packetHeaderHandler.sop = AMCOM_SOP;
+	packetHeaderHandler.type = packetType;
+	packetHeaderHandler.length = payloadSize;
+	packetHeaderHandler.crc = AMCOM_INITIAL_CRC;
+	
+	// create handler to our packet
+	AMCOM_Packet packetHandler;
+	
+	// initialize its header 
+	packetHandler.header = packetHeaderHandler;
+	
+	// calculate CRC
+	packetHeaderHandler.crc = AMCOM_UpdateCRC(packetHeaderHandler.type, packetHeaderHandler.crc);
+	packetHeaderHandler.crc = AMCOM_UpdateCRC(packetHeaderHandler.length, packetHeaderHandler.crc);
+	
+    for (size_t counter = 0; counter < payloadSize; ++counter)
+    {
+        packetHeaderHandler.crc = AMCOM_UpdateCRC(((uint8_t*)payload)[counter], packetHeaderHandler.crc);
+    }
+	
+	// write header to our destination buffer
+	memcpy(destinationBuffer, &packetHeaderHandler, sizeof(AMCOM_PacketHeader));
+	
+	// write actual data to our destination buffer but move to appropriate place in memory
+	memcpy(destinationBuffer + sizeof(AMCOM_PacketHeader), payload, payloadSize);
+        
+    // return information about bytes we write to destination buffer
+    return payloadSize + sizeof(AMCOM_PacketHeader);
+}
+
+void AMCOM_Deserialize(AMCOM_Receiver* receiver, const void* data, size_t dataSize) {
+    static uint8_t lowerNibble = 0;
+    static uint8_t higherNibble = 0;
+    uint16_t calculatedCRC = AMCOM_INITIAL_CRC;
+            
+    for (size_t i = 0; i < dataSize; ++i)
+    {
+        // we got some byte from the buffer
+        uint8_t receivedByte = ((uint8_t*)data)[i];
+        
+        
+        // and we have to make a decision about it
+        switch (receiver->receivedPacketState)
+        {
+        case AMCOM_PACKET_STATE_EMPTY:
+            // try to get our SOP
+            if (receivedByte == AMCOM_SOP)
+            {
+                receiver->receivedPacket.header.sop = AMCOM_SOP;
+                //receiver->payloadCounter = 0;
+                receiver->receivedPacketState = AMCOM_PACKET_STATE_GOT_SOP;
+            }
+            break;
+        case AMCOM_PACKET_STATE_GOT_SOP:
+            // try to get type
+            receiver->receivedPacket.header.type = receivedByte;
+            receiver->receivedPacketState = AMCOM_PACKET_STATE_GOT_TYPE;
+            break;
+        case AMCOM_PACKET_STATE_GOT_TYPE:
+            // try to get length
+            if (receivedByte >= 0 && receivedByte <= AMCOM_MAX_PACKET_SIZE)
+            {
+                receiver->receivedPacket.header.length = receivedByte;
+                receiver->receivedPacketState = AMCOM_PACKET_STATE_GOT_LENGTH;
+            }
+            else
+            {
+                receiver->receivedPacketState = AMCOM_PACKET_STATE_EMPTY;
+            }
+            break;
+        case AMCOM_PACKET_STATE_GOT_LENGTH:
+            // try to get lower byte of crc
+            lowerNibble = receivedByte;
+            receiver->receivedPacketState = AMCOM_PACKET_STATE_GOT_CRC_LO;
+            break;
+        case AMCOM_PACKET_STATE_GOT_CRC_LO:
+            // try to get higher byte of crc
+            higherNibble = receivedByte;
+            
+            // write crc to header
+            receiver->receivedPacket.header.crc = ( (higherNibble << 8) | lowerNibble );
+            
+            // check if we have no data in payload
+            if (receiver->receivedPacket.header.length == 0)
+            {
+                receiver->receivedPacketState = AMCOM_PACKET_STATE_GOT_WHOLE_PACKET;  
+                // this goto is needed because otherwise I would be thrown out of the loop with iterator i 
+                goto got_payload;
+            }
+            else
+            {
+                receiver->receivedPacketState = AMCOM_PACKET_STATE_GETTING_PAYLOAD;
+            }
+            break;
+        case AMCOM_PACKET_STATE_GETTING_PAYLOAD:
+            // if we got to there, we need to get data from payload and write it to payload
+            
+            receiver->receivedPacket.payload[receiver->payloadCounter] = receivedByte;
+            receiver->payloadCounter++;
+            
+            // check if we have no more data to retreive
+            if (receiver->payloadCounter == receiver->receivedPacket.header.length)
+            {
+                receiver->receivedPacketState =  AMCOM_PACKET_STATE_GOT_WHOLE_PACKET;
+                goto got_payload;
+            }
+            break;
+            
+            
+    got_payload:
+        case AMCOM_PACKET_STATE_GOT_WHOLE_PACKET:
+            // we got our packet so we're going to calculate CRC
+            
+            calculatedCRC = AMCOM_UpdateCRC(receiver->receivedPacket.header.type, calculatedCRC);
+            calculatedCRC = AMCOM_UpdateCRC(receiver->receivedPacket.header.length, calculatedCRC);
+            
+            for (size_t j = 0; j < receiver->payloadCounter; ++j)
+            {
+                calculatedCRC = AMCOM_UpdateCRC(receiver->receivedPacket.payload[j], calculatedCRC);
+            }
+            
+            
+            
+            
+            // check if our calculations are ok
+            if ( calculatedCRC == receiver->receivedPacket.header.crc )
+            {
+                // if yes, call the user's function
+                receiver->packetHandler(&(receiver->receivedPacket), receiver->userContext);
+            }
+            
+            
+            //receiver->packetHandler(&(receiver->receivedPacket), receiver->userContext);
+            
+            // go back to empty
+            receiver->receivedPacketState = AMCOM_PACKET_STATE_EMPTY;
+            
+            receiver->payloadCounter = 0;
+            //memset(&receiver->receivedPacket, 0, sizeof(AMCOM_Packet));
+            
+            break;
+            
+        }
+    
+    }
+}
---
+++ main.c
@@ -0,0 +1,229 @@
+#undef UNICODE
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "amcom.h"
+#include "amcom_packets.h"
+#include "math.h"
+
+#define DEFAULT_TCP_PORT 	"2001"
+
+float distance(float x_food, float y_food, float x_player, float y_player) {
+	float dist = ((x_player-x_food)*(x_player-x_food)) + ((y_player-y_food)*(y_player-y_food));
+	return sqrt(dist);
+}
+
+float angle(float x_food, float y_food, float x_player, float y_player) {
+	return atan2(y_player - y_food, x_player - x_food) * 180 / 3.14f;
+}
+
+
+
+/**
+ * This function will be called each time a valid AMCOM packet is received
+ */
+void amcomPacketHandler(const AMCOM_Packet* packet, void* userContext) {
+	uint8_t amcomBuf[AMCOM_MAX_PACKET_SIZE];	// buffer used to serialize outgoing packets
+	size_t bytesToSend = 0;						// size of the outgoing packet
+	static int playerCounter;					// just a counter to distinguish player instances
+	SOCKET sock = (SOCKET)userContext;			// socket used for communication with the game client
+
+	// dopisane przeze mnie
+	static AMCOM_NewGameRequestPayload newGameRequestPayload;		
+	static AMCOM_MoveResponsePayload moveResponsePayload;			
+	static AMCOM_PlayerUpdateRequestPayload playerUpdateRequestPayload;	
+	static AMCOM_FoodUpdateRequestPayload foodUpdateRequestPayload;	
+	static AMCOM_MoveRequestPayload moveRequestPayload;
+
+
+	switch (packet->header.type) {
+	case AMCOM_IDENTIFY_REQUEST:
+		printf("Got IDENTIFY.request. Responding with IDENTIFY.response\n");
+		AMCOM_IdentifyResponsePayload identifyResponse;
+		sprintf(identifyResponse.playerName, "krzys%d", playerCounter++);
+		bytesToSend = AMCOM_Serialize(AMCOM_IDENTIFY_RESPONSE, &identifyResponse, sizeof(identifyResponse), amcomBuf);
+		break;
+	case AMCOM_NEW_GAME_REQUEST:
+		printf("Got NEW_GAME.request.\n");
+		// TODO: respond with NEW_GAME.confirmation
+
+		// akceptacja udziału w grze
+		memcpy(&newGameRequestPayload, packet->payload, sizeof(AMCOM_NewGameRequestPayload));
+		printf("Responding with NEW_GAME_RESPONSE\n");
+		bytesToSend = AMCOM_Serialize(AMCOM_NEW_GAME_RESPONSE, NULL, 0, amcomBuf);
+		
+	    break;
+	case AMCOM_PLAYER_UPDATE_REQUEST:
+		printf("Got PLAYER_UPDATE.request.\n");
+		// TODO: use the received information
+		
+		// ładujemy informacje o player update
+		memcpy(&playerUpdateRequestPayload, packet->payload, packet->header.length);
+		
+
+	    break;
+	case AMCOM_FOOD_UPDATE_REQUEST:
+		printf("Got FOOD_UPDATE.request.\n");
+		// TODO: use the received information
+
+		// ładujemy informacje o food update
+		memcpy(&foodUpdateRequestPayload, packet->payload, packet->header.length);
+		
+
+		break;
+	case AMCOM_MOVE_REQUEST:
+		printf("Got MOVE.request.\n");
+		// TODO: respond with MOVE.confirmation
+		memcpy(&moveRequestPayload, packet->payload, packet->header.length);
+
+		
+		if (moveRequestPayload.x < 0 && moveRequestPayload.y < 0) {
+			moveResponsePayload.angle = 3.14f/4;	
+		}
+		else {
+			moveResponsePayload.angle = -3.14f/4;
+		}
+
+		// wysyłamy mu kąt
+		bytesToSend = AMCOM_Serialize(AMCOM_MOVE_RESPONSE, &moveResponsePayload, sizeof(AMCOM_MoveResponsePayload), amcomBuf);
+
+		break;
+	}
+
+	if (bytesToSend > 0) {
+		int bytesSent = send(sock, (const char*)amcomBuf, bytesToSend, 0 );
+		if (bytesSent == SOCKET_ERROR) {
+			printf("Socket send failed with error: %d\n", WSAGetLastError());
+			closesocket(sock);
+			return;
+		} else {
+			printf("Sent %d bytes through socket.\n", bytesSent);
+		}
+	}
+}
+
+DWORD WINAPI playerThread( LPVOID lpParam )
+{
+	AMCOM_Receiver amcomReceiver;		// AMCOM receiver structure
+	SOCKET sock = (SOCKET)(lpParam);	// socket used for communication with game client
+	char buf[512];						// buffer for temporary data
+	int receivedBytesCount;				// holds the number of bytes received via socket
+
+	printf("Got new TCP connection.\n");
+
+	// Initialize AMCOM receiver
+	AMCOM_InitReceiver(&amcomReceiver, amcomPacketHandler, (void*)sock);
+
+	// Receive data from socket until the peer shuts down the connection
+	do {
+		// Fetch the bytes from socket into buf
+		receivedBytesCount = recv(sock, buf, sizeof(buf), 0);
+		if (receivedBytesCount > 0) {
+			printf("Received %d bytes in socket.\n", receivedBytesCount);
+			// Try to deserialize the incoming data
+			AMCOM_Deserialize(&amcomReceiver, buf, receivedBytesCount);
+		} else if (receivedBytesCount < 0) {
+			// Negative result indicates that there was socket communication error
+			printf("Socket recv failed with error: %d\n", WSAGetLastError());
+			closesocket(sock);
+			break;
+		}
+	} while (receivedBytesCount > 0);
+
+	printf("Closing connection.\n");
+
+	// shutdown the connection since we're done
+	receivedBytesCount = shutdown(sock, SD_SEND);
+	// cleanup
+	closesocket(sock);
+
+	return 0;
+}
+
+int main(int argc, char **argv) {
+	WSADATA wsaData;						// socket library data
+	SOCKET listenSocket = INVALID_SOCKET;	// socket on which we will listen for incoming connections
+	SOCKET clientSocket = INVALID_SOCKET;	// socket for actual communication with the game client
+	struct addrinfo *addrResult = NULL;
+	struct addrinfo hints;
+	int result;
+
+	// Say hello
+	printf("mniAM player listening on port %s\nPress CTRL+x to quit\n", DEFAULT_TCP_PORT);
+
+	// Initialize Winsock
+	result = WSAStartup(MAKEWORD(2,2), &wsaData);
+	if (result != 0) {
+		printf("WSAStartup failed with error: %d\n", result);
+		return -1;
+	}
+
+	// Prepare hints structure
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_protocol = IPPROTO_TCP;
+	hints.ai_flags = AI_PASSIVE;
+
+	// Resolve the server address and port
+	result = getaddrinfo(NULL, DEFAULT_TCP_PORT, &hints, &addrResult);
+	if ( result != 0 ) {
+		printf("Function 'getaddrinfo' failed with error: %d\n", result);
+		WSACleanup();
+		return -2;
+	}
+
+	// Create a socket for connecting to server
+	listenSocket = socket(addrResult->ai_family, addrResult->ai_socktype, addrResult->ai_protocol);
+	if (listenSocket == INVALID_SOCKET) {
+		printf("Function 'socket' failed with error: %ld\n", WSAGetLastError());
+		freeaddrinfo(addrResult);
+		WSACleanup();
+		return -3;
+	}
+	// Setup the TCP listening socket
+	result = bind(listenSocket, addrResult->ai_addr, (int)addrResult->ai_addrlen);
+	if (result == SOCKET_ERROR) {
+		printf("Function 'bind' failed with error: %d\n", WSAGetLastError());
+		freeaddrinfo(addrResult);
+		closesocket(listenSocket);
+		WSACleanup();
+		return -4;
+	}
+	freeaddrinfo(addrResult);
+
+	// Listen for connections
+	result = listen(listenSocket, SOMAXCONN);
+	if (result == SOCKET_ERROR) {
+		printf("Function 'listen' failed with error: %d\n", WSAGetLastError());
+		closesocket(listenSocket);
+		WSACleanup();
+		return -5;
+	}
+
+	while (1) {
+		// Accept client socket
+		clientSocket = accept(listenSocket, NULL, NULL);
+		if (clientSocket == INVALID_SOCKET) {
+			printf("Function 'accept' failed with error: %d\n", WSAGetLastError());
+			closesocket(listenSocket);
+			WSACleanup();
+			return -6;
+		} else {
+			// Run a separate thread to handle the actual game communication
+			CreateThread(NULL, 0, playerThread, (void*)clientSocket, 0, NULL);
+		}
+		Sleep(10);
+	}
+
+	// No longer need server socket
+	closesocket(listenSocket);
+	// Deinitialize socket library
+	WSACleanup();
+	// We're done
+	return 0;
+}
